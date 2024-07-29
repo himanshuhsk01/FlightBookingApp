ef5f25052421868b2f5b35f60b700ec85e749ede
Himanshu Shukla <himanshu.hsk02@gmail.com>
2024-07-30 01:20:21 +0530

    admin page

--- Admin.Designer.cs
+++ Admin.Designer.cs
@@ -34,9 +34,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.filter = new System.Windows.Forms.ComboBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,11 +46,11 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 335);
+            this.dataGridView1.Location = new System.Drawing.Point(196, 313);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1234, 437);
+            this.dataGridView1.Size = new System.Drawing.Size(1315, 437);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -150,7 +151,7 @@
             "Madurai",
             "Ranchi",
             "Raipur"});
-            this.comboBox1.Location = new System.Drawing.Point(168, 63);
+            this.comboBox1.Location = new System.Drawing.Point(523, 144);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(177, 37);
             this.comboBox1.TabIndex = 1;
@@ -252,7 +253,7 @@
             "Madurai",
             "Ranchi",
             "Raipur"});
-            this.comboBox2.Location = new System.Drawing.Point(521, 60);
+            this.comboBox2.Location = new System.Drawing.Point(979, 144);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(177, 37);
             this.comboBox2.TabIndex = 2;
@@ -260,7 +261,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(317, 151);
+            this.button1.Location = new System.Drawing.Point(775, 235);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 40);
             this.button1.TabIndex = 3;
@@ -272,7 +273,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(55, 67);
+            this.label1.Location = new System.Drawing.Point(424, 152);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(93, 29);
             this.label1.TabIndex = 4;
@@ -282,50 +283,62 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(375, 65);
+            this.label2.Location = new System.Drawing.Point(833, 149);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(140, 29);
             this.label2.TabIndex = 5;
             this.label2.Text = "Destination";
             // 
-            // label3
+            // label4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(736, 66);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(217, 29);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Date Of Departure";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(654, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Filter : ";
+            // 
+            // filter
+            // 
+            this.filter.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filter.FormattingEnabled = true;
+            this.filter.Items.AddRange(new object[] {
+            "Source_&_Destination",
+            "RangeOfDate",
+            "All"});
+            this.filter.Location = new System.Drawing.Point(778, 66);
+            this.filter.Name = "filter";
+            this.filter.Size = new System.Drawing.Size(292, 34);
+            this.filter.TabIndex = 10;
+            this.filter.Text = "On Basis Of Followings";
+            this.filter.SelectedIndexChanged += new System.EventHandler(this.filter_SelectedIndexChanged);
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(986, 63);
+            this.dateTimePicker1.Location = new System.Drawing.Point(523, 146);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(251, 36);
-            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.Size = new System.Drawing.Size(267, 36);
+            this.dateTimePicker1.TabIndex = 11;
             // 
-            // button2
+            // dateTimePicker2
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(918, 151);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 40);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(979, 146);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(267, 36);
+            this.dateTimePicker2.TabIndex = 12;
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1340, 722);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.filter);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -350,8 +363,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox filter;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
--- Admin.cs
+++ Admin.cs
@@ -13,10 +13,10 @@ namespace FlightBookingApp
 {
     public partial class Admin : Form
     {
-        SqlCommand myCommand;
+        SqlCommand mycommand;
         DataTable dt;
         SqlDataReader dr;
-        
+        public bool flag = false;
         public Admin()
         {
             InitializeComponent();
@@ -24,13 +24,14 @@ namespace FlightBookingApp
 
         private void Admin_Load(object sender, EventArgs e)
         {
-            //Login.mycon.Open();
-            //myCommand = new SqlCommand("select uname,uphone,locations,departure_date,utr from users inner join booking on users.username=booking.username", Login.mycon);
-            //dr = myCommand.ExecuteReader();
-            //dt = new DataTable();
-            //dt.Load(dr);
-            //dataGridView1.DataSource = dt;
-            //Login.mycon.Close();
+           
+            label1.Visible = false;
+            label2.Visible = false;
+            dateTimePicker1.Visible = false;
+            dateTimePicker2.Visible = false;
+            comboBox1.Visible = false;
+            comboBox2.Visible = false;
+            button1.Visible = false;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -40,22 +41,45 @@ namespace FlightBookingApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (comboBox1.Text != comboBox2.Text)
-            {
+           
+                if (flag)
+                {
+                    Login.mycon.Open();
+                    mycommand = new SqlCommand("SELECT u.uname, u.uage, u.ugender, u.uidentity, u.uphone, b.bsource, b.bdestination, b.bdeparture_date, b.pnr, f.fname, f.fprice FROM users u JOIN booking b ON u.username = b.username JOIN flight f ON f.pnr = b.pnr where bsource=@bsource and bdestination=@bdestination", Login.mycon);
+                    mycommand.Parameters.AddWithValue("@bsource",comboBox1.Text);
+                    mycommand.Parameters.AddWithValue("@bdestination", comboBox2.Text);
+                    dr = mycommand.ExecuteReader();
+                    dt = new DataTable();
+                    dt.Load(dr);
+                    dataGridView1.DataSource = dt;
+                    Login.mycon.Close();
+                }
+                else
+                {
+                
                 Login.mycon.Open();
-                string location = comboBox1.Text + "To" + comboBox2.Text;
-                myCommand = new SqlCommand("select uname,uphone,locations,departure_date,utr from users inner join booking on users.username=booking.username where locations=@locations", Login.mycon);
-                myCommand.Parameters.AddWithValue("@locations", location);
-                dr = myCommand.ExecuteReader();
+                mycommand = new SqlCommand("SELECT u.uname, u.uage, u.ugender, u.uidentity, u.uphone, b.bsource, b.bdestination, b.bdeparture_date, b.pnr, f.fname, f.fprice FROM users u JOIN booking b ON u.username = b.username JOIN flight f ON f.pnr = b.pnr WHERE CONVERT(date, SUBSTRING(b.bdeparture_date, 7, 4) + '-' + SUBSTRING(b.bdeparture_date, 4, 2) + '-' + SUBSTRING(b.bdeparture_date, 1, 2)) BETWEEN CONVERT(date, @blower) AND CONVERT(date, @bhigher)", Login.mycon);
+
+                
+                string lowerDate = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+                string higherDate = dateTimePicker2.Value.ToString("yyyy-MM-dd");
+
+                
+                mycommand.Parameters.AddWithValue("@blower", lowerDate);
+                mycommand.Parameters.AddWithValue("@bhigher", higherDate);
+
+     
+                dr = mycommand.ExecuteReader();
                 dt = new DataTable();
                 dt.Load(dr);
                 dataGridView1.DataSource = dt;
+
+                
                 Login.mycon.Close();
             }
-            else
-            {
-                MessageBox.Show("source and destination should not be same");
-            }
+            
+
+           
            
         }
 
@@ -67,6 +91,57 @@ namespace FlightBookingApp
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void filter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (filter.Text == "Source_&_Destination")
+            {
+                //RangeOfDate
+                label1.Visible = true;
+                label2.Visible = true;
+                label1.Text = "Source";
+                label2.Text = "Destination";
+                dateTimePicker1.Visible = false;
+                dateTimePicker2.Visible = false;
+                label1.Visible = true;
+                label2.Visible = true;
+                comboBox1.Visible = true;
+                comboBox2.Visible = true;
+                button1.Visible = true;
+                flag = true;
+
+            }
+            else if (filter.Text == "RangeOfDate")
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+                label1.Text = "From";
+                label2.Text = "To";
+                comboBox1.Visible = false;
+                comboBox2.Visible = false;
+                dateTimePicker1.Visible = true;
+                dateTimePicker2.Visible = true;
+                button1.Visible = true;
+                flag = false;
+
+                
+            }
+            else if(filter.Text == "All")
+            {
+                Login.mycon.Open();
+                mycommand = new SqlCommand("SELECT u.uname, u.uage, u.ugender, u.uidentity, u.uphone, b.bsource, b.bdestination, b.bdeparture_date, b.pnr, f.fname, f.fprice FROM users u JOIN booking b ON u.username = b.username JOIN flight f ON f.pnr = b.pnr where b.bdeparture_date >= @blower and b.bdeparture_date <= @bhigher", Login.mycon);
+                mycommand.Parameters.AddWithValue("@blower", dateTimePicker1.Value.ToShortDateString());
+                mycommand.Parameters.AddWithValue("@bhigher", dateTimePicker2.Value.ToShortDateString());
+                dr = mycommand.ExecuteReader();
+                dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+                Login.mycon.Close();
+
+
+
+            }
         }
     }
 }
